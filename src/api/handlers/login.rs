975e503a93aa9fa1b35d11865af9f945630f670c
Ben Pirt <ben@pirt.co.uk>
2024-05-14 09:44:30 +0100

    Minor refactor of login handler

--- src/api/handlers/login.rs
+++ src/api/handlers/login.rs
@@ -11,16 +11,13 @@ pub async fn login(
 ) -> Result<LoginResponse, String> {
     let result: Result<User, String> = gateway.get_user_by_email(email).await;
 
-    match result {
-        Ok(user) => {
-            if user.validated && user.password == *password {
-                if let Ok(token) = create_token(user.id) {
-                    return Ok(LoginResponse { token });
-                }
+    if let Ok(user) = result {
+        if user.validated && user.password == *password {
+            if let Ok(token) = create_token(user.id) {
+                return Ok(LoginResponse { token });
             }
-
-            Err(String::from("Error logging in"))
         }
-        Err(_) => Err(String::from("Error logging in")),
     }
+
+    return Err(String::from("Error logging in"));
 }
